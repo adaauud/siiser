1bc4d1edca53849fd4d3c952548a0bc20193a083
adaauud <80990309+adaauud@users.noreply.github.com>
2021-08-02 16:45:06 -0700

    Add files via upload

--- localserver/index.php
+++ localserver/index.php
@@ -1,12 +1,8 @@
 <?php
-include_once('login.db.php');
-if(!$_SESSION['username']){
-    header('location:\localserver/login.php');
-}
+$mysqli = mysqli_connect("us-cdbr-east-04.cleardb.com","bd0f0b0d31a624","ab3b2b6a","heroku_e4df9ee799f1a28");
 
-$mysqli = new mysqli("localhost","root","","localserver");
 $username = $_SESSION['username'];
-$sql = "SELECT * FROM register WHERE username='$username'";
+$sql = "SELECT * FROM files WHERE username='$username'";
 $result = mysqli_query($mysqli,$sql);
 $row = mysqli_fetch_assoc($result);
 $code = $row['code'];
@@ -18,23 +14,6 @@ if(isset($_POST['logout'])){
     header('Location: /localserver/login.php');
 die;
 }
-#  File Upload Code
-if(isset($_FILES['file'])){
-    $errors= array();
-    $file_name = $_FILES['file']['name'];
-    $file_size =$_FILES['file']['size'];
-    $file_tmp =$_FILES['file']['tmp_name'];
-    $file_type=$_FILES['file']['type'];
-    $file_ext=strtolower(end(explode('.',$_FILES['file']['name'])));
-    move_uploaded_file($file_tmp,"uploads/".$file_name);
-    echo "Success";
-    $image = $_FILES["file"]["name"]; 
-    $img = "uploads/".$image;
-    $date = date("Y/m/d") ;
-    $sql = "INSERT INTO file (name,size,uploadeddate, uploadedby)
-    VALUES ( '$file_name','$file_size','$date','$code')";
-    mysqli_query($mysqli, $sql);
-    }
 ?>
 <html>
     <head>
@@ -60,11 +39,6 @@ if(isset($_FILES['file'])){
         <div class="searchbar">
             <input type="search" name="search" placeholder="Search by......">
         </div>
-        <?php
-        $mysqli = new mysqli("localhost","root","","localserver");
-		$data = "SELECT * FROM file";
-		$result = mysqli_query($mysqli,$data);
-        ?>
         <table>
             <tr>
                 <th>Name</th>
@@ -73,9 +47,6 @@ if(isset($_FILES['file'])){
                 <th>Uploaded By</th>
                 <th>Download</th>
             </tr>
-            <?php
-		        while ($row = mysqli_fetch_assoc($result)):
-	        ?>
             <tr>
                 <td><?php  echo $row['name']?></td>
                 <td><?php echo $row['size'] ?></td>
@@ -83,9 +54,6 @@ if(isset($_FILES['file'])){
                 <td><?php echo $row['uploadedby']?></td>
                 <td> <a href="uploads/es.pdf" download>Download </a></td>
             </tr>
-            <?php
-        endwhile;
-        ?>
         </table>
     </body>
 </html>
